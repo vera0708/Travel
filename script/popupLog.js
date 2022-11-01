b1fdc0fb5d2c859e7b94171705d2fcf0294197df
Vera Deeva <verasdee@gmail.com>
2022-11-01 18:25:16 +0100

    feat: made popup, begin carousel

--- script/popupLog.js
+++ script/popupLog.js
@@ -3,32 +3,34 @@ var popuplog = document.querySelector(".popup-wrapper");
 var form = popuplog.querySelector("form");
 var logmail = popuplog.querySelector("[name=email]");
 var logpassword = popuplog.querySelector("[name=password]");
-var storage = localStorage.getItem("logmail");
+
+var link = document.querySelector(".register");
+var popsign = document.querySelector(".popsign-wrapper");
 
 button.addEventListener("click", function(evt) {
     evt.preventDefault();
     popuplog.classList.add("log-show");
-    
-    if (storage) {
-        logmail.value = storage;
-        logpassword.focus();
-    } else {
-        logmail.focus();
-    }
+    logmail.focus();
 });
 
-form.addEventListener("submit", function() {
+form.addEventListener("submit", function(evt) {
     if (!logmail.value || !logpassword.value) {
         evt.preventDefault();
         console.log("Enter email and password");
-    } else {
-        localStorage.setItem("logmail", "logmail.value");
-    }
+    } 
 });
 
 window.addEventListener("keydown", function(evt) {
-    console.log(evt.code)
     if (evt.code === "Escape") {
-        popuplog.classList.remove("log-show");
+       if (popuplog.classList.contains("log-show")) {
+         evt.preventDefault();
+         popuplog.classList.remove("log-show");
+       }
     }
+});
+
+link.addEventListener("click", function(evt) {
+    console.log('sign-show');
+    // evt.preventDefault();
+    popsign.classList.add("sign-show");
 });
