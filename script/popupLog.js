5bbb3fb912507f864bf1e336621b89fdc80e9cca
Vera Deeva <verasdee@gmail.com>
2022-11-06 20:10:58 +0100

    feat: open register popup

--- script/popupLog.js
+++ script/popupLog.js
@@ -7,9 +7,12 @@ var logpassword = popuplog.querySelector("[name=password]");
 var link = document.querySelector(".register");
 var popsign = document.querySelector(".popsign-wrapper");
 
+var registerLink = document.querySelector(".register");
+var popupRegister = document.querySelector(".popsign-wrapper");
+
 button.addEventListener("click", function(evt) {
     evt.preventDefault();
-    popuplog.classList.add("log-show");
+    openLogin();
     logmail.focus();
 });
 
@@ -21,11 +24,16 @@ form.addEventListener("submit", function(evt) {
 });
 
 window.addEventListener("keydown", function(evt) {
+    evt.preventDefault();
     if (evt.code === "Escape") {
-       if (popuplog.classList.contains("log-show")) {
-         evt.preventDefault();
-         popuplog.classList.remove("log-show");
-       }
+        var isLoginPopupOpen = isLoginOpen();
+        var isRegisterPopupOpen = isRegisterOpen();
+
+        if (isLoginPopupOpen) {
+            closeLogin();
+        } else if (isRegisterPopupOpen) {
+            popupRegister.classList.remove("log-show");
+        }
     }
 });
 
@@ -34,3 +42,26 @@ link.addEventListener("click", function(evt) {
     // evt.preventDefault();
     popsign.classList.add("sign-show");
 });
+
+registerLink.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    closeLogin();
+    console.log(popupRegister);
+    popupRegister.classList.add("log-show");
+});
+
+function openLogin() {
+    popuplog.classList.add("log-show");
+}
+
+function closeLogin() {
+    popuplog.classList.remove("log-show");
+}
+
+function isLoginOpen() {
+    return popuplog.classList.contains("log-show");
+}
+
+function isRegisterOpen() {
+    return popupRegister.classList.contains("log-show");
+}
